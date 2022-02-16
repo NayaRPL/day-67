b9490fb44b5a68a9bcf295c85d128b55e3fcda96
NayaRPL <naya.mmj@gmail.com>
2022-02-16 20:53:14 +0800

    day 67

---
+++ operator SWITCH.php
@@ -0,0 +1,15 @@
+<?php
+/* SWITCH adalah operator kondisi yang memiliki fungsi sama dengan IF ELSE IF,
+operator ini juga kita oakai untuk kondidi percabnagan lebih dari satu. bedabya jiak IF ELSE 
+mengerjakan tugasnya dengan cara memeriksa statement kondidi yang ada satu persatu maka switch tidak.
+switch akan memeriksa semua nilai statement kondisi bersamaan, lalu menjalankan yang kondisinya sesuai.*/
+$i = 2;
+switch ($i){
+    case 0:
+        echo "i bernilai 0 "; break;
+    case 1:
+        echo " i bernilai 1"; break;
+    case 2 :
+        echo " i bernilai 2"; break;
+}
+?>
